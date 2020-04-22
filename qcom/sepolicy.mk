162e58a51823c723a5df2ac0e21f71f6d251e198
Michael Bestas <mkbestas@lineageos.org>
2020-04-22 04:02:57 +0300

    sepolicy: qcom: Label custom FM Radio property
    Change-Id: I8927185237021a5f0810bed1c0f4f53b15882a34

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -2,5 +2,8 @@
 # This policy configuration will be used by all qcom products
 #
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/conquer/sepolicy/qcom/private
+
 BOARD_SEPOLICY_DIRS += \
     device/custom/sepolicy/qcom/vendor
