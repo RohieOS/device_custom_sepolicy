e7342a67305d5d9456bc3471d4534f7edcce9eae
dianlujitao <dianlujitao@lineageos.org>
2020-05-07 22:43:01 +0800

    common: Expose aux camera {black,white}list props
    Change-Id: I8f6d2fcd498ed78d8404451b33ba830381c36061
    
    sepolicy: Dynamically build trust policy into system/vendor
    
     * Introduce a new board flag TARGET_USES_PREBUILT_VENDOR_SEPOLICY and
       a sepolicy variant: dynamic
     * When TARGET_USES_PREBUILT_VENDOR_SEPOLICY=true, dynamic act as
       private policy, and vendor policy is excluded in order to avoid
       conflicts (it's not integrated to final builds anyway). When the flag
       is not set, dynamic acts as vendor policy to survive from system
       image change i.e. GSI installation.
    
    Change-Id: I8bfd078d6064616c88e2c58a9fa3aa045dddf303
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -2,12 +2,24 @@
 # This policy configuration will be used by all products that
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/custom/sepolicy/common/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/custom/sepolicy/common/dynamic
+endif
+
 BOARD_SEPOLICY_DIRS += \
+    device/custom/sepolicy/common/dynamic \
     device/custom/sepolicy/common/vendor
 
 # Selectively include legacy rules defined by the products
--include device/conquer/sepolicy/legacy-common/sepolicy.mk
+-include device/custom/sepolicy/legacy-common/sepolicy.mk
 
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -2,8 +2,16 @@
 # This policy configuration will be used by all qcom products
 #
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/conquer/sepolicy/qcom/private
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/custom/sepolicy/qcom/private
+
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/custom/sepolicy/qcom/vendor
+endif
