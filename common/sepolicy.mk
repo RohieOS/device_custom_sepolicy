xdroid-CAF/xd_device_xdroid_sepolicy

sepolicy: Move fingerprint 2.0 service to vendor sepolicy
Change-Id: Ifb55a009f4ff87b9a3571b24943b5e9c31f9dcd2

common/sepolicy.mk
@@ -5,3 +5,6 @@
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/conquer/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/conquer/sepolicy/common/vendor

RohieOS/device_custom_sepolicy

Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

common/sepolicy.mk
@@ -5,3 +5,5 @@
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/custom/sepolicy/common/private
 
+BOARD_SEPOLICY_DIRS += \
+    device/custom/sepolicy/common/vendor