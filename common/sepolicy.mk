lite-os/device_lite_sepolicy

sepolicy: Move policy to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from LiteOS
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/lite/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/lite/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lite/sepolicy/common/private

RohieOS/device_custom_sepolicy

Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

common/sepolicy.mk
@@ -2,5 +2,6 @@
 # This policy configuration will be used by all products that
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/custom/sepolicy/common
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/custom/sepolicy/common/private
+
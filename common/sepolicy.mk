c00dbd56e3c4ea82af649cdb1d10753158f4fa62
Ethan Chen <intervigil@gmail.com>
2017-10-18 19:11:22 -0700

    sepolicy: Move policy to platform
    Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -2,5 +2,6 @@
 # This policy configuration will be used by all products that
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/custom/sepolicy/common
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/custom/sepolicy/common/private
+
